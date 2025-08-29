e3d3e3fb29cb4d586686cf25f18f416f4db20def
Vincent Joy <vincentjoyhere@gmail.com>
2025-08-29 12:42:21 +0530

    Added an iOS demo app

---
+++ Examples/WiPeerKitDemo/WiPeerKitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WiPeerKitDemo
+//
+//  Created by Vincent Joy on 29/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/WiPeerKitDemo/WiPeerKitDemo/WiPeerKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiPeerKitDemoApp.swift
+//  WiPeerKitDemo
+//
+//  Created by Vincent Joy on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WiPeerKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
